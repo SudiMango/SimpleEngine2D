eb6316b95ccac4536d3ec005234a4449c8dbb1f3
SudiMango <sudipto.islam2005@gmail.com>
2025-12-21 22:13:23 -0800

    Updated base engine code

--- engine/include/SimpleEngine2D/core/Engine.hpp
+++ engine/include/SimpleEngine2D/core/Engine.hpp
@@ -1,10 +1,45 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include "SimpleEngine2D/core/System.hpp"
+#include "SimpleEngine2D/core/EntityManager.hpp"
+#include "SimpleEngine2D/systems/Input.hpp"
+#include "SimpleEngine2D/systems/Render.hpp"
+#include "SimpleEngine2D/systems/Collision.hpp"
+#include "SimpleEngine2D/systems/Physics.hpp"
+
 namespace simpleengine2d::core {
 
 class Engine {
+
 public:
-    static const char* Name();
+    Engine() = default;
+    ~Engine() = default;
+    
+    void init(const char* title, int width, int height, bool fullscreen);
+    void run();
+
+    void addSystem(System *system);
+
+    EntityManager &getEntityManager() {
+        return EntityManager::getInstance();
+    }
+
+private:
+    bool running = false;
+    SDL_Window *window;
+
+    systems::Input *inputSystem;
+    systems::Render *renderSystem;
+    std::vector<System*> systems;
+
+    float dt = 0.0f;
+    std::chrono::steady_clock::time_point lastFrameTime;
+
+    void clean();
 };
 
 }
--- engine/src/core/Engine.cpp
+++ engine/src/core/Engine.cpp
@@ -1,9 +1,66 @@
 #include "SimpleEngine2D/core/Engine.hpp"
+#include "SimpleEngine2D/components/Camera.hpp"
 
 namespace simpleengine2d::core {
 
-const char* Engine::Name() {
-    return "SimpleEngine2D";
+void Engine::init(const char* title, int width, int height, bool isFullscreen) {
+    std::cout << "Initializing engine..." << std::endl;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, (isFullscreen) ? SDL_WINDOW_FULLSCREEN : 0);
+
+    renderSystem = new simpleengine2d::systems::Render(window, -1, 0);
+    inputSystem = new simpleengine2d::systems::Input(running);
+
+    addSystem(inputSystem);
+    addSystem(renderSystem);
+    addSystem(new simpleengine2d::systems::Collision());
+    addSystem(new simpleengine2d::systems::Physics());
+
+    EntityId camera = EntityManager::getInstance().createEntity();
+    simpleengine2d::components::Camera *cam = new simpleengine2d::components::Camera{};
+    cam->position = {0, 0};
+    cam->zoom = 1;
+    cam->follow = 1;
+    EntityManager::getInstance().addComponent<simpleengine2d::components::Camera>(camera, cam);
+
+    running = true;
+
+    lastFrameTime = std::chrono::steady_clock::now();
+    std::cout << "Engine initialized!" << std::endl;
+}
+
+void Engine::run() {
+
+    for (auto s : systems) {
+        s->init();
+    }
+
+    while (running) {
+        
+        auto currentTime = std::chrono::steady_clock::now();
+        dt = std::chrono::duration<float>(currentTime - lastFrameTime).count();
+        lastFrameTime = currentTime;
+
+        for (auto s : systems) {
+            s->update(dt);
+        }
+    }
+    
+    clean();
+}
+
+void Engine::clean() {
+    for (auto s : systems) {
+        s->clean();
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    std::cout << "Cleaned, exiting..." << std::endl;
+}
+
+void Engine::addSystem(System *system) {
+    systems.push_back(system);
 }
 
 }
