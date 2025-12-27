a2e6363fdc725bae70e4b920c9da339c041cf57f
SudiMango <sudipto.islam2005@gmail.com>
2025-12-26 18:04:21 -0800

    Added tag management

---
+++ engine/include/SimpleEngine2D/core/TagManager.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+#include "EntityManager.hpp"
+
+namespace simpleengine2d::core {
+
+class TagManager {
+
+public:
+    static TagManager &getInstance();
+
+    void createTag(std::string tagName);
+    void destroyTag(std::string tagName);
+
+    void addTag(EntityId entity, std::string tagName);
+    void removeTag(EntityId entity, std::string tagName);
+    bool hasTag(EntityId entity, std::string tagName);
+
+private:
+    TagManager() = default;
+    ~TagManager() = default;
+
+    TagManager(const TagManager&) = delete;
+    TagManager& operator=(const TagManager&) = delete;
+
+    std::vector<std::string> tags;
+    std::map<EntityId, std::vector<std::string>> entityToTags;
+
+};
+
+}
---
+++ engine/src/core/TagManager.cpp
@@ -0,0 +1,66 @@
+#include "SimpleEngine2D/core/TagManager.hpp"
+
+namespace simpleengine2d::core {
+
+TagManager &TagManager::getInstance() {
+    static TagManager instance;
+    return instance;
+}
+
+/**
+ * Tags creation
+ */
+
+void TagManager::createTag(std::string tagName) {
+    auto it = std::find(tags.begin(), tags.end(), tagName);
+    if (it == tags.end()) {
+        tags.push_back(tagName);
+    }
+}
+
+void TagManager::destroyTag(std::string tagName) {
+    auto it = std::find(tags.begin(), tags.end(), tagName);
+    if (it != tags.end()) {
+        tags.erase(it);
+    }
+}
+
+/**
+ * Tag management with entities
+ */
+
+void TagManager::addTag(EntityId entity, std::string tagName) {
+    auto it_e = entityToTags.find(entity);
+    if (it_e != entityToTags.end()) {
+        auto it_v = std::find(it_e->second.begin(), it_e->second.end(), tagName);
+        if (it_v == it_e->second.end()) {
+            it_e->second.push_back(tagName);
+        }
+    } else {
+        entityToTags[entity].push_back(tagName);
+    }
+}
+
+void TagManager::removeTag(EntityId entity, std::string tagName) {
+    auto it_e = entityToTags.find(entity);
+    if (it_e != entityToTags.end()) {
+        auto it_v = std::find(it_e->second.begin(), it_e->second.end(), tagName);
+        if (it_v != it_e->second.end()) {
+            it_e->second.erase(it_v);
+        }
+    }
+}
+
+bool TagManager::hasTag(EntityId entity, std::string tagName) {
+    auto it_e = entityToTags.find(entity);
+    if (it_e != entityToTags.end()) {
+        auto it_v = std::find(it_e->second.begin(), it_e->second.end(), tagName);
+        if (it_v != it_e->second.end()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+}
